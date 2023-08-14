fa57b8fc997d528dafb472712fa6e03acf1f5e36
olanike olatunji <olanikegloria2020@gmail.com>
2023-08-14 12:52:31 +0100

    created transactions

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
     weight_kg DECIMAL(10, 2)
 );
+
+ALTER TABLE animals ADD COLUMN species VARCHAR;
