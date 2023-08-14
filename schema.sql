cc94727832705ab2d936dad3e0c1462d4e10b190
olanike olatunji <olanikegloria2020@gmail.com>
2023-08-14 22:46:26 +0100

    joined tables for vists

--- schema.sql
+++ schema.sql
@@ -32,3 +32,26 @@ ADD COLUMN species_id INTEGER REFERENCES species(id);
 -- Add the owner_id column as a foreign key referencing owners table
 ALTER TABLE animals
 ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    id SERIAL PRIMARY KEY,
+    vet_id INTEGER REFERENCES vets(id),
+    species_id INTEGER REFERENCES species(id),
+    UNIQUE (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    animal_id INTEGER REFERENCES animals(id),
+    vet_id INTEGER REFERENCES vets(id),
+    visit_date DATE,
+    UNIQUE (animal_id, vet_id, visit_date)
+);
+
